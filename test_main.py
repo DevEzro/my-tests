45facf1e8fb7e388195215972098a08283717b26
DevEzro <92102267+DevEzro@users.noreply.github.com>
2024-07-07 13:53:32 +0200

    Delete test_main.py

--- test_main.py
+++
@@ -1,9 +0,0 @@
-from main import suma
-
-def test_suma():
-    #Inserta los párametros al método y comprueba que la suma de ese resultado
-    assert suma(1,2) == 3
-    assert suma(7,54) == 61
-    assert suma(3,1) == 4
-    assert suma(8,9) == 16 #Aquí el test fallará
-    assert suma(8,20) == 28
