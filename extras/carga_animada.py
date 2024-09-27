9ec59011b784f57e4f608df3b4f372ecd93df952
DevEzro <92102267+DevEzro@users.noreply.github.com>
2024-09-27 13:49:52 +0200

    Update carga_animada.py

--- extras/carga_animada.py
+++ extras/carga_animada.py
@@ -1,9 +1,8 @@
 import sys
 
-print("[   ] Esperando a los elementos...", end='', flush=True)
-for i in range(12):  # 12 iteraciones para un total de 4 segundos (3 puntos, cambiando cada 0.33s)
-    puntos = '.' * (i % 3 + 1)  # Genera 1, 2 o 3 puntos
-    sys.stdout.write('\r[{}] Esperando a los elementos...'.format(puntos.ljust(3)))
-    sys.stdout.flush()
-    time.sleep(0.33)  # Cambia los puntos cada 0.33 segundos
-print("\n")
+    print("[   ] Esperando a los elementos...", end='', flush=True) # Muestra el mensaje, sin añadir una linea nueva e inmediatamente
+    for i in range(12):  # Bucle de 12 iteracciones
+        puntos = '.' * (i % 4)  # Muestra los puntos animados de ninguno a los 3
+        sys.stdout.write('\r[{}] Esperando a los elementos...'.format(puntos.ljust(3))) # Indica el ancho de caracteres
+        sys.stdout.flush() # Indica que lo muestre de inmediato
+        time.sleep(0.33)  # Pausa al final de cada iteraccion durante o.33
