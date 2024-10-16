51d543b316a75c66ff6bbccca17c6f4e83bd2e07
DevEzro <davidtestout@outlook.com>
2024-10-16 10:41:47 +0200

    Update rabbit scripts

--- mqtt/rabbitmq/send.py
+++ mqtt/rabbitmq/send.py
@@ -1,13 +1,18 @@
 import pika
 # Inicializa la conexión (bloqueante: hasta que no se completa se queda detenida)
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+
 # Inicializa el canal de comunicación
 channel = connection.channel()
+
 # Declara una cola llamada 'hello'
 channel.queue_declare(queue='hello')
+
 # Envío del mensaje 'Hola mundo!' a través de la cola de destino 'hello'
 channel.basic_publish(exchange='', routing_key='hello', body='Hola mundo')
+
 # Mensaje a enviar impreso por pantalla
 print(" [->] Enviando mensaje 'Hola mundo!'")
+
 # Cierra la conexión
 connection.close()
