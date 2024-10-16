d051016aa5dabb8db7028ec23a98fca6107d417d
DevEzro <davidtestout@outlook.com>
2024-10-16 14:20:10 +0200

    update rabbnitmq send.py

--- mqtt/rabbitmq/send.py
+++ mqtt/rabbitmq/send.py
@@ -1,4 +1,5 @@
 import pika
+
 # Inicializa la conexión (bloqueante: hasta que no se completa se queda detenida)
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 
@@ -8,6 +9,7 @@ channel = connection.channel()
 # Declara una cola llamada 'hello'
 channel.queue_declare(queue='hello')
 body = input("Introduce un mensaje para el destinatario: ")
+
 # Envío del mensaje 'Hola mundo!' a través de la cola de destino 'hello'
 channel.basic_publish(exchange='', routing_key='hello', body=body)
 
