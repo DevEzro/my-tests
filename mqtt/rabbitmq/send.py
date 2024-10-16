aa14f961d8bf4fe2fa1ace07783c69c2218ff6a3
DevEzro <davidtestout@outlook.com>
2024-10-16 12:44:40 +0200

    Update send.py

--- mqtt/rabbitmq/send.py
+++ mqtt/rabbitmq/send.py
@@ -7,12 +7,12 @@ channel = connection.channel()
 
 # Declara una cola llamada 'hello'
 channel.queue_declare(queue='hello')
-
+body = input("Introduce un mensaje para el destinatario: ")
 # Envío del mensaje 'Hola mundo!' a través de la cola de destino 'hello'
-channel.basic_publish(exchange='', routing_key='hello', body='Hola mundo')
+channel.basic_publish(exchange='', routing_key='hello', body=body)
 
 # Mensaje a enviar impreso por pantalla
-print(" [->] Enviando mensaje 'Hola mundo!'")
+print(f" [->] Enviando mensaje {body}")
 
 # Cierra la conexión
 connection.close()
