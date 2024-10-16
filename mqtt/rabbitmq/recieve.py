cf865848384869369ebc26c967cc2b39ab64ba42
DevEzro <davidtestout@outlook.com>
2024-10-16 10:40:45 +0200

    Update rabbit scripts

--- mqtt/rabbitmq/recieve.py
+++ mqtt/rabbitmq/recieve.py
@@ -1,20 +1,26 @@
-#!/usr/bin/env python
 import pika, sys, os
 
 def main():
+    
     # Inicializa la conexión (bloqueante: hasta que no se completa se queda detenida)
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    
     # Inicializa el canal de comunicación
     channel = connection.channel()
-    # Declara una cola llamada hello
+    
+    # Declara una cola llamada 'hello'
     channel.queue_declare(queue='hello')
+    
     # Método al que se llama cada vez que reciba un mensaje acaompañado de un mensaje
     # por pantalla
     def callback(ch, method, properties, body):
-        print(f" [+] Recibido {body}")
+        message = body.decode('utf-8') # Convierte el body de byte string a UTF-8
+        print(f" [+] Mensaje '{message}' recibido")
+    
     # Config. del consumidor para que escuche la cola, use la función anterior y 
     # confirme la recepción del mensaje
     channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    
     # Ciclo de espera del mensaje junto a un mensaje por pantalla
     print(' [-] Esperando a los mensajes...')
     channel.start_consuming()
--- mqtt/rabbitmq/send.py
+++ mqtt/rabbitmq/send.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import pika
 # Inicializa la conexión (bloqueante: hasta que no se completa se queda detenida)
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
@@ -7,8 +6,8 @@ channel = connection.channel()
 # Declara una cola llamada 'hello'
 channel.queue_declare(queue='hello')
 # Envío del mensaje 'Hola mundo!' a través de la cola de destino 'hello'
-channel.basic_publish(exchange='', routing_key='hello', body='Hola mundo!')
+channel.basic_publish(exchange='', routing_key='hello', body='Hola mundo')
 # Mensaje a enviar impreso por pantalla
-print(" [->] Hola mundo!")
+print(" [->] Enviando mensaje 'Hola mundo!'")
 # Cierra la conexión
 connection.close()
