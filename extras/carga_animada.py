1eedaf4d3b7ab6c3f0d5e28f63342df3fe91cd5e
DevEzro <davidtestout@outlook.com>
2024-10-23 08:29:12 +0200

    updated indent

--- extras/carga_animada.py
+++ extras/carga_animada.py
@@ -1,8 +1,8 @@
-import sys
+import sys, time
 
-    print("[   ] Esperando a los elementos...", end='', flush=True) # Muestra el mensaje, sin añadir una linea nueva e inmediatamente
-    for i in range(12):  # Bucle de 12 iteracciones
-        puntos = '.' * (i % 4)  # Muestra los puntos animados de ninguno a los 3
-        sys.stdout.write('\r[{}] Esperando a los elementos...'.format(puntos.ljust(3))) # Indica el ancho de caracteres
-        sys.stdout.flush() # Indica que lo muestre de inmediato
-        time.sleep(0.33)  # Pausa al final de cada iteraccion durante o.33
+print("[   ] Esperando a los elementos...", end='', flush=True) # Muestra el mensaje, sin añadir una linea nueva e inmediatamente
+for i in range(12):  # Bucle de 12 iteracciones
+    puntos = '.' * (i % 4)  # Muestra los puntos animados de ninguno a los 3
+    sys.stdout.write('\r[{}] Esperando a los elementos...'.format(puntos.ljust(3))) # Indica el ancho de caracteres
+    sys.stdout.flush() # Indica que lo muestre de inmediato
+    time.sleep(0.33)  # Pausa al final de cada iteraccion durante o.33
